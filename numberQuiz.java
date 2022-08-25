a9481897ff19d2a3b3fe93d8b7238e75b61707d5
Morishima Takako <tkkhr.0322@outlook.jp>
2022-08-25 21:30:01 +0900

    0825

--- numberQuiz.java
+++ numberQuiz.java
@@ -1,6 +1,6 @@
 //練習4-4
 public class numberQuiz{
-    public staic void main (String[]args){
+    public static void main (String[]args){
         int numbers ={3,4,9};
         System.out.println("1桁の数字を入力してください");
         //ここから後ろは解答確認した
