6f02541cd8e24c2f5ab9e0d4ed03db0be5bd61ea
Morishima Takako <tkkhr.0322@outlook.jp>
2022-08-28 17:33:05 +0900

    0828-4

--- Email2.java
+++ Email2.java
@@ -1,15 +1,13 @@
 //練習5－3
 public class Email2{
     public static void main (String[]args){
-        String title = "title";
         String address = "guest";
         String text = "message";
         email(address,text);
     }
-    //Stringの後ろはなんで？　argsじゃないの？　email()で変数を宣言(4~6行目）するのならどうか？
     public static void email(String address,String text){
         System.out.println(address+"に、以下のメールを送信しました");
-        System.out.println("件名:"+無題);
+        System.out.println("件名:+無題");
          System.out.println("本文:"+text);
     }
     public static void email(String title,String address,String text){
