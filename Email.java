21672c15b5b626b6274746fb2cea9040ee4c13bf
Morishima Takako <tkkhr.0322@outlook.jp>
2022-08-28 15:55:25 +0900

    0828

--- Email.java
+++ Email.java
@@ -1,12 +1,12 @@
 //練習5－2
 public class Email{
     public static void main (String[]args){
-        email();
-    }
-    public static void email(String[]args){
         String title = "title";
         String address = "guest";
         String text = "message";
+        email();
+    }
+    public static void email(String[]args){
         System.out.println(address+"に、以下のメールを送信しました");
         System.out.println("件名:"+title);
          System.out.println("本文:"+text);
