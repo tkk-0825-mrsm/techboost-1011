9ea0a2d86b37acc43f4e59d0692881c9a397593e
Morishima Takako <tkkhr.0322@outlook.jp>
2022-08-28 18:32:32 +0900

    20220828戻り値

---
+++ Area.java
@@ -0,0 +1,24 @@
+//練習5-4 戻り値がよくわからない
+public class Area{
+    public static void main (String []args){
+        double  triangleArea = calcTriangleArea(10.0,5.0);
+        System.out.println(
+            "三角形の面積:"+triangleArea+"㎠"
+        );
+        double circleArea = calcCircleArea(5.0);
+        System.out.println(
+            "円の面積:"+circleArea+"㎠");
+        }
+        public static double calcTriangleArea(double bottom,double height){
+            double area =(bottom*height)/2;
+        return area;
+         }
+        public static double calcCircleArea(double radius){
+            double area=radius*radius*3.14;
+            return area;
+
+        }    
+    }
+
+
+    
