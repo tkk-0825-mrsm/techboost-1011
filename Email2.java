895c45b45450cabcf7f35b12ef63565632180e07
Morishima Takako <tkkhr.0322@outlook.jp>
2022-08-28 17:37:06 +0900

    0828-5

--- Email2.java
+++ Email2.java
@@ -1,4 +1,4 @@
-//練習5－3
+//練習5－3 オーバーロード理解不足
 public class Email2{
     public static void main (String[]args){
         String address = "guest";
@@ -7,12 +7,12 @@ public class Email2{
     }
     public static void email(String address,String text){
         System.out.println(address+"に、以下のメールを送信しました");
-        System.out.println("件名:+無題");
+        System.out.println("件名:無題");
          System.out.println("本文:"+text);
     }
     public static void email(String title,String address,String text){
         System.out.println(address+"に、以下のメールを送信しました");
-        System.out.println("件名:"+無題);
+        System.out.println("件名:"+title);
         System.out.println("本文:"+text);
     }   
 }
