bcbb8896dbe1bcde7f7db53ffcdc3269f14a36fb
Morishima Takako <tkkhr.0322@outlook.jp>
2022-09-01 18:36:52 +0900

    0901-1

--- MitoKomon.java
+++ MitoKomon.java
@@ -3,9 +3,9 @@ import comment.Zenhan;
 
 public class MitoKomon{
     public static void main(String[]args){
-     doWarusa();
-     doTogame();
-     callDeae();
-     showMondokoro();   
+     Zenhan.doWarusa();
+     Zenhan.doTogame();
+     Kohan.callDeae();
+     Kohan.showMondokoro();   
     }   
 }
