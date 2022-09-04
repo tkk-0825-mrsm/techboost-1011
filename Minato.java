e3e0363963daccf8149e4e815d98b967f68009cc
Morishima Takako <tkkhr.0322@outlook.jp>
2022-09-04 19:05:32 +0900

    0904

---
+++ Minato.java
@@ -0,0 +1,9 @@
+public class Minato{
+    public static void main(String[]args){
+        System.out.println("メソットを呼び出します");
+        hello();
+    }
+    public static void hello(){
+        System.out.println("湊さん、こんにちは");
+    }
+}
