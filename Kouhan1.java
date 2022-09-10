4d9fc555749f4fe5786ece9fdc2571e35f0bf831
Morishima Takako <tkkhr.0322@outlook.jp>
2022-09-10 19:53:58 +0900

    20220910-1

--- Kouhan1.java
+++ Kouhan1.java
@@ -8,6 +8,8 @@ public class Kouhan1{
     public static void showMondokoro() throws Exception{
         System.out.println("飛車さん、角さん。もういいでしょう。");
         System.out.println("この紋所が目に入らぬか！");
+        //練習6－3
+        java.lang.Thread.sleep(3000);
         Zenhan1.doTogame();
     }    
 }
