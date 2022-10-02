248cf3d9bd02f6c3dee2109149f709ab45f09714
Morishima Takako <tkkhr.0322@outlook.jp>
2022-10-02 22:05:35 +0900

    1002 文法③課題3

---
+++ ObjectList.java
@@ -0,0 +1,15 @@
+//テックブースト　課題3　(質問の答え写した)
+//ObjectList.java class
+import java.util.ArrayList;
+
+//ArrayListのコード　<>の中はなんでＴなの？
+public class ObjectList<T> extends ArrayList<T> {
+
+    public boolean add(T value) {
+		return super.add(value);
+	}
+
+	public T get(int index) {
+		return super.get(index);
+	}
+}
--- TextReader.java
+++ TextReader.java
@@ -1,63 +1,54 @@
-//テックブースト　文法3 課題3
+//テックブースト　文法3 課題3 列挙型以外は理解できてない
 //TextReader class
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.*;
 
 public class TextReader {
-    private String path;
+	private String path;
 
-    private static Logger logger = Logger.getLogger(TextReader.class.getName());
+	private static Logger logger = Logger.getLogger(TextReader.class.getName());
+    
+    //列挙型ってことはenum?
+	public enum Mark {START, END
+	};
 
-    private static final String START = "START";
-    private static final String END = "END";
+	private TextReader(String path) {
+		this.path = path;
+	}
 
-    private TextReader(String path) {
-        this.path = path;
-    }
-
-    private void execute() {
-        List valueList = new ArrayList();
-
-        FileReader fr = null;
-        BufferedReader br = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-            String line;
-            while ((line = br.readLine()) != null) {
-                valueList.add(new Integer(line));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                }
-            }
-            if (fr != null) {
-                try {
-                    fr.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-        int size = valueList.size();
-        for (int i = 0; i < size; i++) {
-            System.out.println(valueList.get(i));
-        }
-    }
-
-    public static void main(String[] args) {
-        logger.info(START);
-        new TextReader(args[0]).execute();
-        logger.info(END);
-    }
+	private void execute() {
+        //TODO: この `valueList` 変数はIntegerのインスタンスしか追加しないので、総称型を利用したObjectListを使うように書き換え
+        //回答よくわからない　Integerどこからきた？
+		List<Integer> valueList = new ObjectList<>();
+        
+        // TODO: リソース付き try 文を使う
+        //()内にはcloseによる後片付けが必要な変数の宣言　本来の処理
+		try (FileReader fr = new FileReader(path);BufferedReader br =
+         new BufferedReader(fr)) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				valueList.add(Integer.parseInt(line));
+			}
+    
+        //例外処理
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        // TODO: 拡張 for 文を利用
+		for (Integer value : valueList) {
+			System.out.println(value);
+		}
+	}
+   
+    // TODO: ここで可変長引数:ピリオドを3つ並べる
+	public static void main(String...args) {
+		logger.info(Mark.START.name());
+		new TextReader(args[0]).execute();
+		logger.info(Mark.END.name());
+	}
 
 }
