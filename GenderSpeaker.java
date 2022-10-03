84ddf2d91abd546595383094e21c81db5dc2e755
Morishima Takako <tkkhr.0322@outlook.jp>
2022-10-03 21:11:24 +0900

    1003メンタリング

--- GenderSpeaker.java
+++ GenderSpeaker.java
@@ -2,11 +2,17 @@
 //GenderSpeaker class(処理実行クラス)
 public class GenderSpeaker{
     public static void main(String[]args){
-        doSpeak(new Man());
+//dospeakメソットを呼び出す　メソット名();でメソット名を呼び出す
+    //     
+        doSpeak(new Man()); //インスタンスを作ってそれを引数として渡す　Man型のインスタンス
 		doSpeak(new Woman());
 	}
 
-	private static void doSpeak(AbstractPerson person) {
-		person.speak();
+	//引数を引き取っている()の中の型にあった引数を渡す　publicはどこからでも呼び出せる　誰が見れるか使えるか
+	//親クラスの型を受け取っている　manとwomanは継承元(親classが一緒)が同じなのでこれ1つでまかなえる
+	public static void doSpeak(AbstractPerson person) {
+		
+		//インスタンス.メソット名();でメソットを呼び出す
+		person.speak(); //Man classの
 }
 }
--- TryCatchFinallySample.java
+++ TryCatchFinallySample.java
@@ -1,14 +1,25 @@
 //テックブースト　文法2 課題2
 public class TryCatchFinallySample {
 
-	public static void main(String[] args) {
+	//引数をcatchしている　
+	//どんなデータかわからないものを引数として引き取っている(確定してない情報を引き取る)
+	public static void main(String[] args) { //文字を入れるための配列
+		
+		//tryの中に処理を書く
 		try{
 			System.out.println("try");
-			Integer.parseInt(args[0]);
+			
+			//Integer.parseInt:もともとjavaに入ってるメソット　String型からint型に変換するもの
+			Integer.parseInt(args[0]);	//[]引きだし状の箱　配列を意味する　0なのでいちばん最初の配列
+		
+			//catch:tryの中で問題が起きた時の処置(エラーにならないように、強制終了しないための処置) 
+		//エラーメッセージとかでユーザーを誘導する処理　エラーで落ちないように
 		} catch (Exception e) {
-			System.out.println("catch");
+			System.out.println("catch"); //エラーについてのメッセージを残す　ログを残す≒保険をかけている　思わぬ事態に対応
+		
+		//tryの処理が終わったら絶対にやりたい処理　省略してもOK(特にやりたいことがなければ)
 		} finally {
-			System.out.println("finally");
+			System.out.println("finally"); //ex)開いたファイルを閉じる処理、データベースの接続の遮断、解除
 		}
 	}
 }
