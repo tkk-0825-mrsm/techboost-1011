e9ea51df74c0f31e2c9606c35dada6f10533c3ac
Morishima Takako <tkkhr.0322@outlook.jp>
2022-10-11 19:10:31 +0900

    1011文法３課題④

--- Lambda.java
+++ Lambda.java
@@ -1,5 +1,6 @@
 //テックブースト　文法3課題4
 //ラムダ式で現在の時間 (HH:mm:ss.SSS) と半角スペースと 1 から 9999 の連番
+//オンサポで質問済。回答教えてもらった　ラムダ式はよくわからない
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.stream.IntStream;
@@ -9,8 +10,8 @@ public class Lambda {
 
 	public static void main(String[] args) {
 		IntStream stream = IntStream.range(1, 10000);
-        //ラムダ式
-		stream.forEach(FORMATTER -> System.out.println(③));
+        //ラムダ式 sの値
+		stream.forEach(s -> System.out.println(FORMATTER.format(LocalDateTime.now())+ " "+ s));
 	}
 
 	private static String currentTime() {
